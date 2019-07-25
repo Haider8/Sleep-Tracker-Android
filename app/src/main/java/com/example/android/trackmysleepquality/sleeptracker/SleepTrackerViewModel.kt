dcc137f4342cc46ba8759e1e32c1ba72a0927c6a
Haider Ali <haider.lee23@gmail.com>
2019-07-25 18:00:15 +0530

    Navigate to sleep quality when stop button is clicked

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -17,7 +17,9 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -47,6 +49,14 @@ class SleepTrackerViewModel(
 
 	val nightsString = Transformations.map(nights) { nights ->
 		formatNights(nights, application.resources)
+	}
+
+	private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+	val navigateToSleepQuality: LiveData<SleepNight>
+		get() = _navigateToSleepQuality
+
+	fun doneNavigating() {
+		_navigateToSleepQuality.value = null
 	}
 
 	init {
@@ -89,6 +99,7 @@ class SleepTrackerViewModel(
 			val oldNight = tonight.value ?: return@launch
 			oldNight.endTimeMilli = System.currentTimeMillis()
 			update(oldNight)
+			_navigateToSleepQuality.value = oldNight
 		}
 	}
 
